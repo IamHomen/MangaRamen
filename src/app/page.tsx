171393a8e2c1f8c1e579693eae499bb8a4afc809
Renato Dacanay (Homen) <masterzense16@gmail.com>
2025-04-21 07:35:05 +0000

    ⨯ useSearchParams() should be wrapped in a suspense boundary at page "/". Read more: https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout     at a (/vercel/path0/.next/server/chunks/169.js:5:40696)     at f (/vercel/path0/.next/server/chunks/169.js:3:19640)     at l (/vercel/path0/.next/server/app/page.js:1:1834)     at nL (/vercel/path0/node_modules/next/dist/compiled/next-server/app-page.runtime.prod.js:76:46773)     at nF (/vercel/path0/node_modules/next/dist/compiled/next-server/app-page.runtime.prod.js:76:48548)     at nq (/vercel/path0/node_modules/next/dist/compiled/next-server/app-page.runtime.prod.js:76:67434)     at nH (/vercel/path0/node_modules/next/dist/compiled/next-server/app-page.runtime.prod.js:76:65009)     at nV (/vercel/path0/node_modules/next/dist/compiled/next-server/app-page.runtime.prod.js:76:70865)     at nz (/vercel/path0/node_modules/next/dist/compiled/next-server/app-page.runtime.prod.js:76:69548)     at nq (/vercel/path0/node_modules/next/dist/compiled/next-server/app-page.runtime.prod.js:76:67701) Error occurred prerendering page "/". Read more: https://nextjs.org/docs/messages/prerender-error Export encountered an error on /page: /, exiting the build.  ⨯ Next.js build worker exited with code: 1 and signal: null Error: Command "npm run build" exited with 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,16 @@
 
 import MangaListing from "@/components/MangaListing";
 import Header from "@/components/Header";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <div className="container mx-auto py-10 px-4">
-        <MangaListing />
+        <Suspense fallback={<p>Loading...</p>}>
+          <MangaListing />
+        </Suspense>
       </div>
     </div>
   );
